589f965e0c135d0c0b52ca5ae46784dfc1f75b7d
Bhupendra Singh <bhupendrasingh.ec18@gmail.com>
2021-08-22 21:08:38 +0530

    create an add data API via Post

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  addData(value: string): any {
+    const array = [];
+    if (!value) {
+      throw new NotFoundException('Please provide inputs')
+    }
+    array.push(value);
+    return { ...array };
   }
 }
